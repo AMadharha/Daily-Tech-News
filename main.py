38d7f439c265035f5ed89addf4458db285c6997c
Ankush <ankushmadharha@gmail.com>
2022-08-08 21:33:56 -0400

    tweet code implemented

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tweepy
 import praw
 
-all_keys = open("keys", "r").read().splitlines()
+allKeys = open("keys", "r").read().splitlines()
 
 # Twitter Authentication
-API_KEY = all_keys[0].strip()
-API_KEY_SECRET = all_keys[1].strip()
-ACCESS_TOKEN = all_keys[2].strip()
-ACCESS_TOKEN_SECRET = all_keys[3].strip()
+API_KEY = allKeys[0].strip()
+API_KEY_SECRET = allKeys[1].strip()
+ACCESS_TOKEN = allKeys[2].strip()
+ACCESS_TOKEN_SECRET = allKeys[3].strip()
 
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET) 
@@ -16,11 +16,13 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # Reddit Authentication
 reddit = praw.Reddit(client_id = "lzGC0n5GmClEoK_gdSitAA",
-                     client_secret = all_keys[4].strip(),
+                     client_secret = allKeys[4].strip(),
                      username = "DailyTechNewsTweet",
-                     password = all_keys[5].strip(),
+                     password = allKeys[5].strip(),
                      user_agent = "DailyTechNews")
 
 # Tweet Code
 subreddit = reddit.subreddit("technews")
-top = subreddit.top("day", limit = 1)
+top = subreddit.top(time_filter = "day", limit = 1)
+for post in top:
+    api.update_status("#DTN\n{" + post.title + "}\n" + post.url)
