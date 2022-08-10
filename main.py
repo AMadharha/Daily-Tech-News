b4bd8e519021a48bd9e69a69fd9ba2b1fb3f1987
Ankush <ankushmadharha@gmail.com>
2022-08-09 23:28:05 -0400

    apple and technology subreddits added

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ reddit = praw.Reddit(client_id = "lzGC0n5GmClEoK_gdSitAA",
                      user_agent = "DailyTechNews")
 
 # Tweet Code
-subreddit = reddit.subreddit("technews")
+subreddit = reddit.subreddit("technews+apple+technology")
 top = subreddit.top(time_filter = "day", limit = 1)
 for post in top:
     api.update_status("#DTN\n[" + post.title + "]\n" + post.url)
