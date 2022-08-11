9ba8d5a05355b29ca005c81fe10f403239c46ffb
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-11 15:14:02 -0400

    get paragrapgs from <p> tags

--- keywords.py
+++ keywords.py
@@ -2,5 +2,13 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def keywords:
+def keywords():
+    def getdata(url):
+        r = requests.get(url)
+        return r.text
     
+    htmldata = getdata("https://www.newsweek.com/ceos-linkedin-crying-selfie-about-layoffs-backlash-1732677")
+    soup = BeautifulSoup(htmldata, "html.parser")
+    data = ""
+    for data in soup.find_all("p"):
+        print(data.get_text())
