896bf43ef0881f773a82b91e409f6b627ea7d47c
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-11 11:39:50 -0400

    keyword function

--- keywords.py
+++ keywords.py
@@ -2,4 +2,5 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-htmldata = getdata(https://www.newsweek.com/ceos-linkedin-crying-selfie-about-layoffs-backlash-1732677)
+def keywords:
+    
