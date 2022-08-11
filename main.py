4f63b4be86777b1ba9920438df04df0d27578744
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-11 11:19:36 -0400

    authentication is now in auth.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import tweepy
 import praw
 import config
+import auth
 
 # Twitter Authentication
-auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
-auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET) 
+auth.authTwitter()
 
 # Reddit Authentication
-reddit = praw.Reddit(client_id = "lzGC0n5GmClEoK_gdSitAA",
-                     client_secret = config.CLIENT_SECRET,
-                     username = "DailyTechNewsTweet",
-                     password = config.PASSWORD,
-                     user_agent = "DailyTechNews")
+auth.authReddit()
 
 # Tweet Code
 api = tweepy.API(auth, wait_on_rate_limit=True)
