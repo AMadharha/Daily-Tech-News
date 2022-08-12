3513099b6fceca392c75e4ac421c7a708c94f610
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-12 13:19:59 -0400

    URL and duplication verification

--- main.py
+++ main.py
@@ -13,11 +13,25 @@ reddit = authentication.authReddit()
 subreddit = reddit.subreddit("apple+technology")
 top = subreddit.top(time_filter = "day", limit = 1)
 
+# URL and duplication verification
+status = api.user_timeline(screen_name="DailyTechnoNews", count=1)
+if(status):
+    prev_tweet = status[0].text[status[0].text.find('[')+1:status[0].text.find(']')]
+    substring = "www.reddit.com"
+    for post in top:
+        if (substring in post.url) or (prev_tweet.strip() == post.title.strip()):
+            subreddit = reddit.subreddit("technews")
+            new_top = subreddit.top(time_filter="day", limit = 1)
+        else:
+            new_top = subreddit.top(time_filter = "day", limit = 1)
+else:
+    new_top = subreddit.top(time_filter = "day", limit = 1)
+
 # Get keywords list
-for post in top:
+article = ""
+for post in new_top:
     article = keywords.get_article(post.url)
 keyword_list = keywords.get_keywords(post.title + ", " + article)
-print(article)
 
 # Create Tweet
 hashtags = ""
@@ -26,8 +40,8 @@ for kw in keyword_list:
 tweet = "#DTN #DailyTechNews\n[" + post.title + "]\n" + hashtags + "\n" + post.url
 
 # Tweet 
-#api.update_status(tweet)
+api.update_status(tweet)
 
 # Like own recent tweet
-#status = api.user_timeline(screen_name="DailyTechnoNews", count=1)
-#api.create_favorite(status[0].id)
+status = api.user_timeline(screen_name="DailyTechnoNews", count=1)
+api.create_favorite(status[0].id)
