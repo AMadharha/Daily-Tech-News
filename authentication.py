2240e3176f136b719572367e89587c7cfec357a8
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-12 02:13:24 -0400

    environment variables

--- authentication.py
+++ authentication.py
@@ -1,18 +1,27 @@
 import tweepy
 import praw
 import config
+from boto.s3.connection import S3Connection
+import os
+
+s3 = S3Connection(os.environ["API_KEY"], 
+                  os.environ["API_KEY_SECRET"],
+                  os.environ["ACCESS_TOKEN"],
+                  os.environ["ACCESS_TOKEN_SECRET"],
+                  os.environ["CLIENT_SECRET"],
+                  os.environ["PASSWORD"])
 
 # Twitter authentication
 def authTwitter():
-    auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
-    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET) 
+    auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET")) 
     return auth
 
 # Reddit Authentication
 def authReddit():
     reddit = praw.Reddit(client_id = "lzGC0n5GmClEoK_gdSitAA",
-                     client_secret = config.CLIENT_SECRET,
+                     client_secret = os.getenv("CLIENT_SECRET"),
                      username = "DailyTechNewsTweet",
-                     password = config.PASSWORD,
+                     password = os.getenv("PASSWORD"),
                      user_agent = "DailyTechNews")
     return reddit
