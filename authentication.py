405a38d774a5d28bf4e5dc269f88c1428ad1ed78
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-12 00:07:47 -0400

    comments added

--- authentication.py
+++ authentication.py
@@ -2,11 +2,13 @@ import tweepy
 import praw
 import config
 
+# Twitter authentication
 def authTwitter():
     auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET) 
     return auth
 
+# Reddit Authentication
 def authReddit():
     reddit = praw.Reddit(client_id = "lzGC0n5GmClEoK_gdSitAA",
                      client_secret = config.CLIENT_SECRET,
