5d7e89b1cdb0ecc79106c4c7babbecbe6b7130ff
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-18 14:55:12 -0400

    result_type changed

--- like.py
+++ like.py
@@ -6,7 +6,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 search_results = api.search_tweets(q="science", 
                                    lang="en", 
-                                   result_type="mixed",
+                                   result_type="recent",
                                    count=1)
 
 result = search_results[0]
