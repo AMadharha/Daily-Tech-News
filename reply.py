8c88f68058d641dbd14972ededd3e1d5d7e793b4
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-25 08:12:17 -0400

    improved reply code

--- reply.py
+++ reply.py
@@ -1,6 +1,7 @@
 import tweepy
 import Authentication
 from praw.models import MoreComments
+import sys
 
 def reply_to_user(user, subreddit):
     auth = Authentication.authTwitter()
@@ -10,17 +11,14 @@ def reply_to_user(user, subreddit):
     subreddit = reddit.subreddit(subreddit)
     top_list_generator = subreddit.top(time_filter="day", limit=1)
     submission = next(top_list_generator)
-    top_score = -10000
+    top_score = -sys.maxsize - 1
     top_comment = ""
     for top_level_comment in submission.comments:
         if isinstance(top_level_comment, MoreComments):
             continue
-        if top_level_comment.score > top_score:
+        if (top_level_comment.score > top_score) and (len(top_level_comment.body) < 270):
             top_comment = top_level_comment.body
             top_score = top_level_comment.score
-
-    if len(top_comment) > 275:
-        top_comment = top_comment[0:150] + "..."
 
     status = api.user_timeline(screen_name=user, count=1, exclude_replies=True)
     api.update_status(status="@" + user + " " + top_comment, in_reply_to_status_id=status[0].id)
@@ -29,8 +27,5 @@ def reply_to_user(user, subreddit):
     api.create_favorite(status[0].id) 
 
 reply_to_user("elonmusk", "teslamotors")
-reply_to_user("sundarpichai", "google")
-reply_to_user("tim_cook", "apple")
-reply_to_user("REALMizkif", "Mizkif")
-reply_to_user("nmplol", "Nmpx")
-reply_to_user("pokelawls", "pokelawls")
+reply_to_user("Google", "google")
+reply_to_user("Apple", "apple")
